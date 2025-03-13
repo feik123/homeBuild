3f04fc89abf1651fa600e2ed8211b0008f159726
feik123 <feik@abv.bg>
2025-03-13 15:31:09 +0200

    feat: added a message model

--- homeBuild/common/models.py
+++ homeBuild/common/models.py
@@ -69,3 +69,15 @@ class ContractorApplication(models.Model):
 
     def __str__(self):
         return f"{self.contractor} - {self.job.title}"
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(to=UserModel, on_delete=models.CASCADE, related_name='sent_messages')
+    receiver = models.ForeignKey(to=UserModel, on_delete=models.CASCADE, related_name='received_messages')
+    job = models.ForeignKey(to=Job, on_delete=models.CASCADE, related_name='messages')
+    content = models.TextField()
+    date_sent = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default=False)  # To track if the message has been read
+
+    def __str__(self):
+        return f"Message from {self.sender} to {self.receiver} for {self.job.title}"
